6c017c2bdb7bacd7d8d2d09be6976ea0b1f42ff8
axsk <smartmind23@googlemail.com>
2018-05-16 17:10:26 +0200

    add wrapper to old cluster functionality
    to reuse code based on DataFrames
    move plot code to data (since its rather data specific)
    remove metastability/coupling computation since P is not accessible

--- src/Hokusai.jl
+++ src/Hokusai.jl
@@ -6,42 +6,8 @@ using Clustering
 using PyPlot, Colors
 using Optim, StatsBase
 
-type HokusaiResult
-    data
-    assignments
-    n
-    tau
-    sigma
-    P
-end
-
 include("pccap.jl")
 include("cluster.jl")
-include("plot.jl")
-include("api.jl")
-
-
-# TODO: move these diagnostics somewhere meaningful
-
-metastability(result) = trace(coupling(result)) / result.n
-
-function coupling(result)
-    P = zeros(result.n, result.n)	
-    by(result.data, :groupby) do subjdata	
-	for row = 1:size(subjdata,1)-1
-	    i = result.assignments[subjdata[row, :index]]
-	    j = result.assignments[subjdata[row+1, :index]]
-	    P[i,j] += 1
-	end
-    end
-    rownormalize(P)
-end
-
-
-try
-    readdata!()
-catch
-    warn("could not load data")
-end
+include("data.jl")
 
 end
--- src/data.jl
+++ src/data.jl
@@ -1,9 +1,23 @@
-datapath = joinpath(@__DIR__, "..", "data")
-
-global data
+type HokusaiResult
+    data
+    assignments
+    n
+    tau
+    sigma
+    P
+end
 
 function readdata!(datafile = joinpath(datapath, "sallsac_Hokusai.seq"))
     global data = readtable(datafile, separator = '\t')
+end
+
+# initialize data
+datapath = joinpath(@__DIR__, "..", "data")
+global data
+try
+    readdata!()
+catch
+    warn("could not load data")
 end
 
 # TODO: add filters for mirrored, starting location, ..
@@ -21,7 +35,7 @@ function filterdata(data::DataFrame, image)
 
     # scale/filter coordinates to 0->width, 0->height
     width, height = data[1,:width], data[1,:height]
-    dx, dy = (1280-width)/2, (1024-height)/2    
+    dx, dy = (1280-width)/2, (1024-height)/2
     data[:fposx] = data[:fposx] - dx
     data[:fposy] = data[:fposy] - dy
     data = data[((data[:fposx] .> 0) .& (data[:fposy] .> 0) .& (data[:fposx] .< width) .& (data[:fposy] .< height)) , :]
@@ -51,19 +65,14 @@ function savecl(i, n, tau, sigma; method=:scaling, precl=0, overwrite=false, fol
 
     result = Hokusai.cluster(imgdata[[:fposx, :fposy, :fixdur, :subj]], n, tau=tau, sigma=sigma, precluster=precl, sort=:size, method=method, symmetrize=symmetrize)
 
-
     imgfile = string(i)[1]
 
     PyPlot.figure()
     PyPlot.axis(:off)
     Hokusai.plot(result, joinpath(datapath, "$imgfile.jpg"), imgdata[1,:width], imgdata[1,:height])
 
-    W = Hokusai.coupling(result)
-    metastab = round(trace(W)*100/n, 2)
-    minstab  = round(minimum(diag(W))*100, 2)
-
     if caption
-        PyPlot.text(0,0,"img=$i n=$n tau=$tau sigma=$sigma\nmethod=$method precl=$precl metastab=$metastab% minstab=$minstab")
+        PyPlot.text(0,0,"img=$i n=$n tau=$tau sigma=$sigma\nmethod=$method precl=$precl")
     end
 
     println("saving image")
@@ -73,4 +82,39 @@ function savecl(i, n, tau, sigma; method=:scaling, precl=0, overwrite=false, fol
     # was used to store metadata
     #push!(df, [i n tau sigma method precl metastab minstab])
     return result
+end
+
+# legacy cluster wrapper
+function cluster(data::DataFrame, n; tau=50, sigma=100, precluster=0, sort=:size, method=:scaling, symmetrize=false)
+    ts = TimeSeries[]
+    by(data, :subj) do d
+        times = cumsum(d[:fixdur])
+        points = hcat(d[:fposx], d[:fposy])
+        push!(ts, TimeSeries(times, points))
+    end
+    ass = cluster(ts, n, tau=tau, sigma=sigma, precluster=precluster, sort=sort, method=method, symmetrize=symmetrize)
+
+    data[:cluster] = ass
+    HokusaiResult(data, ass, n, tau, sigma, nothing)
+end
+
+function plot(result::HokusaiResult, imagepath::String, width, height)
+    data = result.data
+    img = PyPlot.imread(imagepath)
+
+    PyPlot.imshow(img, extent=(0,width,height,0))
+
+    colors = distinguishable_colors(maximum(Array(data[:cluster])), lchoices=linspace(60, 255, 5))
+    #lchoices guarantees a miminmal luminance here
+
+    i=0
+    by(data, :cluster) do data
+	i+=1
+	PyPlot.plot(Array(data[:x]), Array(data[:y]),
+		    "o", markerfacecolor=(colors[i].r, colors[i].g, colors[i].b), alpha=1)
+    end
+
+    PyPlot.autoscale(tight=true)
+
+    return plt
 end
